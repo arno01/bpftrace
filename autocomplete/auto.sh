4b2239fbd091350d8d72467115a2742eaf75ab03
williangaspar <williangaspar360@gmail.com>
2018-12-18 19:16:09 -0200

    better probe completion

--- autocomplete/auto.sh
+++ autocomplete/auto.sh
@@ -4,29 +4,44 @@
 
 _words_complete()
 {
-    local cur prev result opts
+    local cur prev opts
     COMPREPLY=()
     cur="${COMP_WORDS[COMP_CWORD]}"
     prev="${COMP_WORDS[COMP_CWORD-1]}"
+
     opts="-l -e -p -v -d -dd"
-    probes="kprobe kretprobe uprobe uretprobe tracepoint ustd profile interval software hardware"
+    probes="kprobe kretprobe uprobe uretprobe tracepoint usdt profile interval software hardware"
 
     local no_quote_cur=$(echo "${cur}" | sed 's/\x27//g')
 
     if [[ ${cur} == -* ]] ; then
         COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
         return 0
-    elif [[ ${cur} =~ kprobe* ]] ; then
-        local result=$(sudo bpftrace -l | grep "${no_quote_cur}")
-        COMPREPLY=( $(compgen -W "${result}" -- ${cur}) )
+    elif [[ ${cur} =~ kprobe* || ${cur} =~ tracepoint* ]] ; then
+        local kprobe_list=$(./bpftrace -l | grep "${no_quote_cur}")
+        COMPREPLY=( $(compgen -W "${kprobe_list}" -- ${cur}) )
         return 0
-    elif [[ ${cur} =~ tracepoint* ]] ; then
-        local result=$(sudo bpftrace -l "tracepoint:*" | grep "${no_quote_cur}")
-        COMPREPLY=( $(compgen -W "${result}" -- ${cur}) )
+    elif [[ ${cur} =~ kretprobe* ]] ; then
+        local replaced_cur=$(echo "${no_quote_cur}" | sed 's/kretprobe/kprobe/g')
+        local kretprobe_list=$(./bpftrace -l | grep "${replaced_cur}")
+        local comp=$(compgen -W "${kretprobe_list}" -- ${replaced_cur})
+        COMPREPLY=$( echo "${comp}" | sed 's/kprobe/kretprobe/g' )
+        return 0
+    elif [[ ${cur} =~ uprobe* ]] ; then
+        local no_probe_cur=$(echo "${no_quote_cur}" | sed -E 's/uprobe://g')
+        COMPREPLY=( $(compgen -P uprobe: -G "${no_probe_cur}*") )
+        return 0
+    elif [[ ${cur} =~ uretprobe* ]] ; then
+        local no_probe_cur=$(echo "${no_quote_cur}" | sed -E 's/uretprobe://g')
+        COMPREPLY=( $(compgen -P uretprobe: -G "${no_probe_cur}*") )
+        return 0
+    elif [[ ${cur} =~ usdt* ]] ; then
+        local no_probe_cur=$(echo "${no_quote_cur}" | sed -E 's/usdt://g')
+        COMPREPLY=( $(compgen -P usdt: -G "${no_probe_cur}*") )
         return 0
     else
         COMPREPLY=( $(compgen -W "${probes}" -- ${cur}) )
         return 0
     fi
 }
-complete -o nospace -o noquote -F _words_complete bpftrace
+complete -o nospace -F _words_complete bpftrace
